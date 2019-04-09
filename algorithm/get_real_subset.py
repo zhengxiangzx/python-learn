b34e7abaefaeb1854225617f39cddb02034b236f
zhengxiangpx <zhengxiangpx@gmail.com>
2019-04-09 23:22:46 +0800

    添加求一个集合的非空子集

---
+++ algorithm/get_real_subset.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+def get_subset(subset, result=[]):
+    if len(subset) <= 1:
+        return None
+    for i in range(len(subset)):
+        arr = []
+        for num in subset:
+            if num != subset[i]:
+                arr.append(num)
+        get_subset(arr, result)
+        # list.count(obj) 用于统计obj元素在list中出现的次数
+        if result.count(arr) == 0:
+            result.append(arr)
+    return result
+
+
+if __name__ == '__main__':
+    list1 = [1, 2, 3, 4]
+    list2 = get_subset(list1)
+    list2.sort()
+    print(list2)
